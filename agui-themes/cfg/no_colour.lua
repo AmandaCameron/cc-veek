906c9795f19d47af8c4e70b73b66b356059bca21
Amanda Cameron <git@amanda.darkdna.net>
2014-05-15 14:35:35 -0400

    Add seperator to the mono theme.

--- agui-themes/cfg/no_colour.lua
+++ agui-themes/cfg/no_colour.lua
@@ -9,26 +9,34 @@
 		['button-fg'] = 'black',
 		['button-bg'] = 'white',
 
-        ['list-item-focused-bg'] = 'white',
-        ['list-item-focused-fg'] = 'black',
+                ['input-fg'] = 'black',
+                ['input-bg'] = 'white',
 
-        ['tab-focused-active-fg'] = 'black',
-        ['tab-focused-active-bg'] = 'white',
+                ['input-focused-fg'] = 'input-fg',
+                ['input-focused-bg'] = 'input-bg',
 
+                ['list-item-focused-bg'] = 'white',
+                ['list-item-focused-fg'] = 'black',
 
-        ['progress-bar-compl-bg'] = 'white',
-        ['progress-bar-compl-fg'] = 'black',
+                ['tab-focused-active-fg'] = 'black',
+                ['tab-focused-active-bg'] = 'white',
 
-        ['progress-bar-fg'] = 'white',
-        ['progress-bar-bg'] = 'black',
+                ['progress-bar-compl-bg'] = 'white',
+                ['progress-bar-compl-fg'] = 'black',
 
-        ['progress-bar-ind-fg'] = 'black',
-        ['progress-bar-ind-bg'] = 'white',
+                ['progress-bar-fg'] = 'white',
+                ['progress-bar-bg'] = 'black',
 
-        ['menuitem-selected-fg'] = 'black',
-        ['menuitem-selected-bg'] = 'white',
+                ['progress-bar-ind-fg'] = 'black',
+                ['progress-bar-ind-bg'] = 'white',
 
-        ['menuitem-bg'] = 'white',
-        ['menuitem-fg'] = 'black',
+                ['menuitem-selected-fg'] = 'black',
+                ['menuitem-selected-bg'] = 'white',
+
+                ['menuitem-bg'] = 'white',
+                ['menuitem-fg'] = 'black',
+
+                ['seperator-fg'] = 'white',
+                ['seperator-bg'] = 'white',
 	}
 }
