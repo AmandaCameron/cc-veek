b2fcdb11f3140185f46b7e113dce9fb5976f8138
Amanda Cameron <git@amanda.darkdna.net>
2014-05-22 12:32:45 -0400

    Initial pass of making agui-scroll-view

--- lib-agui/lib/widgets/list.lua
+++ lib-agui/lib/widgets/list.lua
@@ -1,25 +1,42 @@
 -- List Widget! It's what's for dinner!
 
--- Previously this inherited from container, but that prooved far too problemattic.
-_parent = 'agui-widget'
+_parent = 'agui-scroll-view'
 
 function Widget:init(x, y, width, height)
-	self.agui_widget:init(x, y, width, height)
-
-	self.offset = 0
+	self.agui_scroll_view:init(width, height)
 
 	self.items = {}
 	self.current_item = 0
 
-	self.agui_widget:add_flag('active')
-
-	self.agui_widget.bg = 'list--bg'
 	self.agui_widget.fg = 'list--fg'
+	self.agui_widget.bg = 'list--bg'
+
+	self.agui_scroll_view.draw_contents = function(c)
+		self:draw_contents(c)
+	end
+
+	self.agui_scroll_view.get_size = function()
+		return self:get_size()
+	end
+end
+
+-- Scrollable stuff.
+
+function Widget:get_size()
+	local height = 0
+
+	for _, item in ipairs(self.items) do
+		height = height + item:cast('agui-widget').height
+	end
+
+	return self.agui_widget.width, height
 end
 
 -- Helpers.
 
 function Widget:mark_dirty()
+	self.agui_widget.dirty = true
+
 	for _, item in ipairs(self.items) do
 		item.agui_widget.dirty = true
 	end
@@ -30,6 +47,8 @@ end
 function Widget:clear()
 	self.current_item = 0
 	self.items = {}
+
+	self:reflow()
 end
 
 function Widget:add(item, pos)
@@ -47,22 +66,17 @@ function Widget:add(item, pos)
 		self.current_item = #self.items
 		self:get_current():focus()
 	end
+
+	self:reflow()
 end
 
 function Widget:remove(item)
 	table.remove(self.items, item)
+
+	self:reflow()
 end
 
 -- Manupulation of state
-
-function Widget:scroll_up()
-	self.offset = math.min(0, self.offset - math.floor(self.agui_widget.height / 3))
-end
-
-function Widget:scroll_down()
-	self.offset = math.max(self.agui_widget.height - math.floor(self.agui_widget.height / 3) - 1,
-		self.offset + math.floor(self.agui_widget.height / 3))
-end
 
 function Widget:move_up()
 	if #self.items == 0 then
@@ -77,13 +91,9 @@ function Widget:move_up()
 
 	if self.current_item < 1 then
 		self.current_item = #self.items
-
-		self.offset = math.max(#self.items - self.agui_widget.height, 0)
 	end
 
-	if self.current_item - self.offset <= 0 then
-		self:scroll_up()
-	end
+	self:scroll_into_view(0, self:get_current():cast('agui-widget').y - 1)
 
 	self:get_current():focus()
 	
@@ -103,13 +113,9 @@ function Widget:move_down()
 
 	if self.current_item > #self.items then	
 		self.current_item = 1
-
-		self.offset = 0
 	end
 
-	if (self.current_item * self:get_current().agui_widget.height) - self.offset > self.agui_widget.height then
-		self:scroll_down()
-	end
+	self:scroll_into_view(0, self:get_current():cast('agui-widget').y - 1)
 
 	self:get_current():focus()
 
@@ -126,9 +132,7 @@ end
 -- Main Loop Callbacks.
 
 function Widget:focus()
-	self.agui_widget:focus()
-
-	self:mark_dirty ()
+	self:mark_dirty()
 
 	if self:get_current() then
 		self:get_current():focus()
@@ -136,39 +140,51 @@ function Widget:focus()
 end
 
 function Widget:blur()
-	self.agui_widget:blur()
-
 	self:mark_dirty()
 end
 
 function Widget:key(k)
-	if k == keys.pageUp then
-		self:scroll_up()
-	elseif k == keys.pageDown then
-		self:scroll_down()
-	elseif k == keys.up then
+	if k == keys.up then
 		self:move_up()
+
+		return true
 	elseif k == keys.down then
 		self:move_down()
+
+		return true
+	elseif self.agui_scroll_view:key(k) then
+
+		return true
 	elseif self:get_current() then
-		self:get_current():key(k)
+
+		return self:get_current():key(k)
 	end
+	
+	return false
 end
 
-function Widget:draw(c, theme)
-	c:clear()
-
-	c:translate(0, 0 - self.offset)
-
+function Widget:reflow()
 	local y = 1
 
-	for n, item in ipairs(self.items) do
+	for _, item in ipairs(self.items) do
 		item.agui_widget.width = self.agui_widget.width
 		item.agui_widget.x = 1
 		item.agui_widget.y = y
 
 		y = y + item.agui_widget.height
+	end
 
+	self.agui_scroll_view:reflow()
+end
+
+function Widget:resize(w, h)
+	self.agui_widget:resize(w, h)
+
+	self:reflow()
+end
+
+function Widget:draw_contents(c, theme)
+	for n, item in ipairs(self.items) do
 		if n ~= self.current_item then
 			self:draw_raw(item, c, theme)
 		end
